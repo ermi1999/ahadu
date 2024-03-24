1bc3c47bfa0e7e6ed70a075a8f1a976996fedcce
Ermiyas <ermiyasabiye1999@gmail.com>
2024-03-24 22:44:05 +0300

    closures upvalues

--- compiler.c
+++ compiler.c
@@ -54,6 +54,11 @@ typedef struct {
   int depth;
 } Local;
 
+typedef struct {
+  uint8_t index;
+  bool isLocal;
+} Upvalue;
+
 typedef enum {
   TYPE_FUNCTION,
   TYPE_SCRIPT
@@ -66,6 +71,7 @@ typedef struct Compiler{
 
   Local locals[UINT8_COUNT];
   int localCount;
+  Upvalue upvalues[UINT8_COUNT];
   int scopeDepth;
 } Compiler;
 
@@ -329,6 +335,44 @@ static int resolveLocal(Compiler *compiler, Token *name) {
 }
 
 /**
+ * addUpvalue - adds an upvalue.
+ * @compiler: the compiler to add the upvalue to.
+ * @index: the index of the upvalue.
+ * @isLocal: true if the upvalue is local, false otherwise.
+ * @return: the index of the upvalue.
+ */
+static int addUpvalue(Compiler *compiler, uint8_t index, bool isLocal) {
+  int upvalueCount = compiler->function->upvalueCount;
+
+  for (int i = 0; i < upvalueCount; i++) {
+    Upvalue *upvalue = &compiler->upvalues[i];
+    if (upvalue->index == index && upvalue->isLocal == isLocal) {
+      return i;
+    }
+  }
+
+  if (upvalueCount == UINT8_COUNT) {
+    error("Too many closure variables in function.");
+    return 0;
+  }
+
+  compiler->upvalues[upvalueCount].isLocal = isLocal;
+  compiler->upvalues[upvalueCount].index = index;
+  return compiler->function->upvalueCount++;
+}
+
+static int resolveUpvalue(Compiler *compiler, Token *name) {
+  if (compiler->enclosing == NULL) return -1;
+
+  int local = resolveLocal(compiler->enclosing, name);
+  if (local != -1) {
+    return addUpvalue(compiler, (uint8_t)local, true);
+  }
+
+  return -1;
+}
+
+/**
  * addLocal - adds a local variable.
  * @name: the name of the variable.
  */
@@ -413,6 +457,9 @@ static void namedVariable(Token name, bool canAssign) {
   if (arg != -1) {
     getOp = OP_GET_LOCAL;
     setOp = OP_SET_LOCAL;
+  } else if ((arg = resolveUpvalue(current, &name)) != -1) {
+    getOp = OP_GET_UPVALUE;
+    setOp = OP_SET_UPVALUE;
   } else {
     arg = identifierConstant(&name);
     getOp = OP_GET_GLOBAL;
